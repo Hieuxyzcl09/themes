04a15ef2459ff35e218b9f970ada2be57403f66f
Hieuxyzcl09 <165543181+Hieuxyzcl09@users.noreply.github.com>
2024-08-29 21:30:02 +0700

    Create CosmicNight.js

---
+++ CosmicNight.js
@@ -0,0 +1,183 @@
+{
+    "name": "CosmicNight",
+    "description": "A cosmic-themed Discord experience",
+    "authors": [
+        {
+            "name": "hieuxyz",
+            "id": "994819569795485747"
+        }
+    ],
+    "plus": {
+        "version": 1,
+        "mentionLineColor": "#00FFFF",
+        "iconpack": "fluent-icons-cosmos"
+    },
+    "semanticColors": {
+        "HEADER_PRIMARY": [
+            "#00FFFF"
+        ],
+        "HEADER_SECONDARY": [
+            "#FF00FF"
+        ],
+        "TEXT_NORMAL": [
+            "#FFFFFF"
+        ],
+        "TEXT_PRIMARY": [
+            "#FFFFFF"
+        ],
+        "TEXT_MUTED": [
+            "#8A2BE2"
+        ],
+        "INTERACTIVE_NORMAL": [
+            "#00FFFF"
+        ],
+        "INTERACTIVE_HOVER": [
+            "#FF00FF"
+        ],
+        "INTERACTIVE_ACTIVE": [
+            "#FF1493"
+        ],
+        "INTERACTIVE_MUTED": [
+            "#4B0082"
+        ],
+        "BACKGROUND_PRIMARY": [
+            "#000033"
+        ],
+        "BACKGROUND_SECONDARY": [
+            "#000066"
+        ],
+        "BACKGROUND_SECONDARY_ALT": [
+            "#000099"
+        ],
+        "BACKGROUND_TERTIARY": [
+            "#0000CC"
+        ],
+        "BACKGROUND_ACCENT": [
+            "#4B0082"
+        ],
+        "BACKGROUND_FLOATING": [
+            "#191970"
+        ],
+        "BACKGROUND_MOBILE_PRIMARY": [
+            "#000033"
+        ],
+        "BACKGROUND_MOBILE_SECONDARY": [
+            "#000066"
+        ],
+        "BACKGROUND_NESTED_FLOATING": [
+            "#4B0082"
+        ],
+        "BACKGROUND_MENTIONED": [
+            "rgba(0, 255, 255, 0.4)"
+        ],
+        "BACKGROUND_MESSAGE_HOVER": [
+            "#4B0082"
+        ],
+        "BACKGROUND_MODIFIER_HOVER": [
+            "#191970"
+        ],
+        "BACKGROUND_MODIFIER_ACTIVE": [
+            "#000099"
+        ],
+        "BACKGROUND_MODIFIER_SELECTED": [
+            "#0000CC"
+        ],
+        "BACKGROUND_MODIFIER_ACCENT": [
+            "#4B0082"
+        ],
+        "SCROLLBAR_THIN_THUMB": [
+            "#00FFFF"
+        ],
+        "SCROLLBAR_THIN_TRACK": [
+            "transparent"
+        ],
+        "SCROLLBAR_AUTO_THUMB": [
+            "#00FFFF"
+        ],
+        "SCROLLBAR_AUTO_TRACK": [
+            "#191970"
+        ],
+        "CHANNEL_TEXT_AREA_PLACEHOLDER": [
+            "#8A2BE2"
+        ],
+        "CHANNELS_DEFAULT": [
+            "#8A2BE2"
+        ],
+        "TEXT_LINK": [
+            "#FF00FF"
+        ],
+        "TEXT_DANGER": [
+            "#FF1493"
+        ],
+        "KEYBOARD": [
+            "#191970",
+            "#FFFFFF"
+        ],
+        "CHANNEL_ICON": [
+            "#00FFFF"
+        ],
+        "CHAT_BACKGROUND": [
+            "#000033"
+        ],
+        "REDESIGN_ACTIVITY_CARD_BACKGROUND": [
+            "#000066"
+        ],
+        "CARD_PRIMARY_BG": [
+            "#000099"
+        ],
+        "CARD_SECONDARY_BG": [
+            "#0000CC"
+        ],
+        "REDESIGN_BUTTON_SECONDARY_BACKGROUND": [
+            "#191970"
+        ],
+        "BG_BACKDROP": [
+            "04010173"
+        ]
+    },
+    "rawColors": {
+        "PRIMARY_500": "#00FFFF",
+        "PRIMARY_100": "#FF00FF",
+        "PRIMARY_200": "#FF1493",
+        "PRIMARY_230": "#FFFFFF",
+        "PRIMARY_300": "#8A2BE2",
+        "PRIMARY_360": "#4B0082",
+        "PRIMARY_400": "#00FFFF",
+        "PRIMARY_600": "#000033",
+        "PRIMARY_630": "#000066",
+        "PRIMARY_645": "#000099",
+        "PRIMARY_660": "#0000CC",
+        "PRIMARY_700": "#191970",
+        "PRIMARY_800": "#000033",
+        "PLUM_1": "#FFFFFF",
+        "PLUM_3": "#00FFFF",
+        "PLUM_6": "#FF00FF",
+        "PLUM_9": "#FFFFFF",
+        "PLUM_10": "#8A2BE2",
+        "PLUM_11": "#000033",
+        "PLUM_13": "#4B0082",
+        "PLUM_17": "#000066",
+        "PLUM_18": "#000099",
+        "PLUM_19": "#0000CC",
+        "PLUM_20": "#191970",
+        "PLUM_22": "#4B0082",
+        "PLUM_24": "#000033",
+        "BRAND_260": "#FF1493",
+        "BRAND_360": "#4B0082",
+        "BRAND_500": "#00FFFF",
+        "BRAND_560": "#FF00FF",
+        "YELLOW_300": "#FFD700",
+        "YELLOW_260": "#FFA500",
+        "GREEN_600": "#00FF00",
+        "GREEN_360": "#00FA9A",
+        "RED_400": "#FF0000",
+        "WHITE_500": "#FFFFFF"
+    },
+    "background": {
+        "blur": 0.7,
+        "url": "https://images.unsplash.com/photo-1506318137071-a8e063b4bec0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+        "alpha": 0.8
+    },
+    "color": "#00FFFF",
+    "spec": 2
+}
